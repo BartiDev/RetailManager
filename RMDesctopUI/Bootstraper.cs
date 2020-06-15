b24ca4039c19f03acbee64a1a39a7ea2b89bdaa2
Bartlomiej Nowak <bartlomiej.nowak22@gmail.com>
2020-06-15 21:46:40 +0200

    Add Desctop User Interface to Solution
    Adds new WPF project to solution and adds Caliburn NuGet.

---
+++ RMDesctopUI/Bootstraper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using RMDesctopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesctopUI
+{
+    public class Bootstraper : BootstrapperBase
+    {
+        public Bootstraper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
